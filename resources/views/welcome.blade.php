692778856f7042c1deb552b3693e8175fec65d26
Olgierd Dzimiński <o.dziminski@gmail.com>
2020-08-04 09:00:03 +0200

    Add padding, remove surplus Bootstrap classes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,12 +21,12 @@
         <style>
             html,
             body {
-                background-color: #fff;
-                color: #636b6f;
+                height: calc(100%) !important;
+                background-color: white;
                 font-family: "Lato", sans-serif;
                 font-weight: 200;
                 height: 100vh;
-                margin: 0;
+                padding-top: 3%;
             }
 
             .full-height {
@@ -70,6 +70,8 @@
             .m-b-md {
                 margin-bottom: 30px;
             }
+
+            
         </style>
     </head>
 
@@ -85,26 +87,27 @@
 
             @endif
             <form action="/shorten" method="POST">
-                <div class="form-group">
+                <div class="form-group row justify-content-center">
                     @csrf
-                    <div class="input-group">
-                        <input type="urlInput" name="original_link" class="form-control w-50 p-3 container d-inline-block" placeholder="paste your link to shorten" />
+                    <div class="input-group w-50 p-3">
+                        <input type="url" name="original_link" class="form-control" placeholder="paste your link to shorten" />
                         <button type="submit" class="btn btn-primary">shrink!</button>
                     </div>
                 </div>
             </form>
+          
 
             @if (isset($short_link))
-            <div class="input-group card-body w-25 p-3 container">
-                <input type="text" value="{{$short_link}}" id="output" class="form-control-plaintext w-25 p-3 border" />
-                <button onclick="copyToClipboard()" class="btn btn-primary border">
+            <div class="input-group card-body w-25 p-3 container ">
+                <input type="text" value="{{$short_link}}" class="form-control-plaintext form-rounded w-25 p-3 border " />
+                <button onclick="copyToClipboard()" class="btn btn-primary border form-rounded">
                     <svg width="2em" viewBox="0 0 16 16" class="bi bi-clipboard" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd" d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z" />
                         <path fill-rule="evenodd" d="M9.5 1h-3a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z" />
                     </svg>
                 </button>
             </div>
-            <p class="text-primary">Visited {{$visited_counter ?? ''}} times</p>
+            <p class="text-primary">Visited {{$visited_counter}} times</p>
         </div>
         @endif
     </body>
